3f77ec108604fa81a35371aee35db368386f5fa4
ogechukwu25 <Emmanuela Nnuta>
2023-02-24 14:33:05 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,11 @@ int main(void)
 
 	printf("infinite loop incoming :(\n");
 	i = 0;
-	while (i < 10)
-	{
-	putchar(i);
-
-	}
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*i++;*/
+	/*}*/
 	printf("infinite loop avioded! \\o/\n");
 	return (0);
 }
