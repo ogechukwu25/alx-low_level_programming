3c39ba2364a0f648608b385faee22e4ec734bd77
ogechukwu25 <Emmanuela Nnuta>
2023-03-13 08:30:24 -0700

    the static library libmy.a

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - C file to be used
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+_puts("\"Finally, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
+}
